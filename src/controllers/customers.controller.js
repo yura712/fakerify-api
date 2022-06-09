5e270dc32e449883026847cd9e7293fa931df633
Yura Bobrov <skm.yura@gmail.com>
2022-06-09 23:18:15 +0300

    update customers controller

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -1,6 +1,15 @@
+const { faker } = require('@faker-js/faker');
+
 class CustomersController {
   static getCustomers(ctx) {
-    ctx.body = ['test', 'one more'];
+    ctx.response.body = Array.from(Array(100).keys())
+      .map((value, index) => ({
+        id: index + 1,
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.unique(faker.internet.email),
+        phone: faker.phone.phoneNumber('+37529-###-##-##'),
+      }));
   }
 }
 
--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -2,11 +2,12 @@ const { faker } = require('@faker-js/faker');
 
 class ProductsController {
   static getProducts(ctx) {
-    ctx.response.body = Array.from(Array(10)
+    ctx.response.body = Array.from(Array(100)
       .keys())
       .map(() => ({
         title: faker.lorem.words(2),
         description: faker.lorem.paragraph(),
+        price: faker.random.numeric(2),
       }));
   }
 }
